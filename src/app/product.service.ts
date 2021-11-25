75ed134c39326091207a49c02d8d13d54f9d06c0
Abednego-py <71936420+Abednego-py@users.noreply.github.com>
2021-11-25 11:42:57 +0100

    new  commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 // import rxjs/add/operator/map
 import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http'
-
+import {Http, Response} from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +9,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json'
   constructor(private _http: Http) {}
   
-    getAlbum(id : number) : any {
-      return this._http.get(this._albumUrl).forEach(response => response.json())
+    getAlbum(id : number) {
+      return this._http.get(this._albumUrl).pipe(map(response => response.json()))
     }
 
 }
